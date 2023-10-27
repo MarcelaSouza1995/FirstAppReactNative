891c6b396db550354f6e71fc590581303c5b16ef
marcela.souza <marcela9908@gmail.com>
2023-10-27 14:46:44 -0300

    feature: Adição de arquivo de cores

---
+++ src/styles/GlobalColors.ts
@@ -0,0 +1,7 @@
+export const primaryColor = '#03045e';
+export const secondColor = '#023e8a';
+export const thirdcolor = '#0077b6';
+export const fourthColor = '#0096c7';
+export const primaryBackgroundColor = '#caf0f8';
+export const secondBackgroundColor = '#ade8f4';
+export const thirdBackgroundColor = '#90e0ef';
